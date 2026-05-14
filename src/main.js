b7e890ac7677295b6718921d797d3758995faaa1
小樹苗工作室 <92939444+r3111453@users.noreply.github.com>
2026-05-14 15:27:47 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -8,6 +8,3 @@ const app = createApp(App)
 const pinia = createPinia()
 
 app.use(pinia).use(router).mount('#app')
-// 在檔案最下面加上這兩行
-import App from './App.vue'
-console.log('main.js 有執行，App 是：', App)
